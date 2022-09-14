5b7197d25d63f79f2186d21d8e199007fe2c933a
Daniel8529 <97201981+Daniel8529@users.noreply.github.com>
2022-09-13 22:47:41 -0400

    d

--- app/src/main/java/com/ucne/registroprestamos/util/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.ucne.registroprestamos.util
-
-sealed class Screen(val route: String){
-    object OcupacioneListScreen: Screen("OcupacioneList")
-    object OcupacioneScreen: Screen("Ocupacione")
-}
